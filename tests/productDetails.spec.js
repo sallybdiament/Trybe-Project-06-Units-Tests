4814d0cabdc7d8d77e0215ba5151a98428c08cf0
Sally Diament <sally.barzilai@gmail.com>
2022-07-05 19:54:22 -0300

    incluindo o projeto

---
+++ tests/productDetails.spec.js
@@ -0,0 +1,54 @@
+const productDetails = require('../src/productDetails');
+/*
+  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara')
+
+  // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  Escreva pelo menos cinco testes para essa função para garantir que a implementação de productDetails está correta.
+
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    expect(typeof productDetails).toBe('function');
+    const resultado = productDetails('a', 'b');
+    const verificaArray = Array.isArray(resultado);
+    expect(verificaArray).not.toBeFalsy();
+    expect(resultado.length).toEqual(2);
+    expect(typeof resultado[0]).toBe('object');
+    expect(typeof resultado[1]).toBe('object');
+    expect(resultado[1]).not.toBe(resultado[0]);
+    const chaveProductId1 = resultado[0].details.productId;
+    const chaveProductId2 = resultado[1].details.productId;
+    expect(chaveProductId1).toMatch('123');
+    expect(chaveProductId2).toMatch('123');
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste se productDetails é uma função.
+    // Teste se o retorno da função é um array.
+    // Teste se o array retornado pela função contém dois itens dentro.
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    // Teste se os dois productIds terminam com 123.
+  });
+});
